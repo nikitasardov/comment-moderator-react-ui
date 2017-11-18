7b1f3111dded0c5a070650810daa1e45d72c1e20
nsardov <nsardov@gmail.com>
2017-11-18 15:29:33 +0400

    try to fix mixed content

--- src/libs/API.js
+++ src/libs/API.js
@@ -2,7 +2,7 @@
 
 export default class API {
     constructor() {
-        this.baseURL = 'http://91.201.41.52:4567/api';
+        this.baseURL = '//91.201.41.52:4567/api';
         this.getArticlesURL = `${this.baseURL}/articles/`;
         this.getArticleURL = `${this.baseURL}/articles/`;
 
